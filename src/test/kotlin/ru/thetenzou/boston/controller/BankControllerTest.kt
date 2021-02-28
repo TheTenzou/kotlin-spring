31d7dfe9a7a8dc90d26619bc7f83960d2b071678
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-02-28 10:52:25 +0300

    Update controller test with nested tests

--- src/test/kotlin/ru/thetenzou/boston/controller/BankControllerTest.kt
+++ src/test/kotlin/ru/thetenzou/boston/controller/BankControllerTest.kt
@@ -1,7 +1,10 @@
 package ru.thetenzou.boston.controller
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -16,28 +19,40 @@ internal class BankControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    @Test
-    fun `should return all banks`() {
-        mockMvc.get("/api/banks")
-            .andDo { print() }
-            .andExpect {
-                status { isOk() }
-                content { contentType((MediaType.APPLICATION_JSON)) }
-                jsonPath("$[0].accountNumber") { value("123") }
-            }
+    @Nested
+    @DisplayName("getBanks()")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class GetBanks {
+
+        @Test
+        fun `should return all banks`() {
+            mockMvc.get("/api/banks")
+                .andDo { print() }
+                .andExpect {
+                    status { isOk() }
+                    content { contentType((MediaType.APPLICATION_JSON)) }
+                    jsonPath("$[0].accountNumber") { value("123") }
+                }
+        }
     }
 
-    @Test
-    fun `should return bank with given account number`() {
-        val accountNumber = 123
+    @Nested
+    @DisplayName("getBank()")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class GetBank {
 
-        mockMvc.get("/api/banks/$accountNumber")
-            .andDo { print() }
-            .andExpect {
-                status { isOk() }
-                content { contentType(MediaType.APPLICATION_JSON) }
-                jsonPath("$.trust") { value("2.0") }
-                jsonPath("$.transactionFee") { value("1") }
-            }
+        @Test
+        fun `should return bank with given account number`() {
+            val accountNumber = 123
+
+            mockMvc.get("/api/banks/$accountNumber")
+                .andDo { print() }
+                .andExpect {
+                    status { isOk() }
+                    content { contentType(MediaType.APPLICATION_JSON) }
+                    jsonPath("$.trust") { value("2.0") }
+                    jsonPath("$.transactionFee") { value("1") }
+                }
+        }
     }
 }
